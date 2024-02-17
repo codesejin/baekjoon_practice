9b013565d467068969fffb373cd8d2801f56395f
sejin park <seijin0722@naver.com>
2024-02-17 12:03:21 +0900

    :memo: LV_Silver

--- silver/그리디/회의실배정/Main.java
+++ silver/그리디/회의실배정/Main.java
@@ -1,2 +1,46 @@
-package silver.그리디.회의실배정;public class Main {
+package silver.그리디.회의실배정;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Meeting[] meetings = new Meeting[N];
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            meetings[i] = new Meeting(Integer.parseInt(input[0]), Integer.parseInt(input[1]));
+        }
+
+        Arrays.sort(meetings);
+        int endTime = 0, cnt = 0;
+        for (Meeting meeting : meetings) {
+            if (meeting.start >= endTime) {
+                endTime = meeting.end;
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+
+}
+
+class Meeting implements Comparable<Meeting> {
+    int start;
+    int end;
+
+    Meeting(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public int compareTo(Meeting o) {
+        if (this.end == o.end) {
+            return this.start - o.start; // 오름차순(음수)
+        } else return this.end - o.end;
+    }
 }
