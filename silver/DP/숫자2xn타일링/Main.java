a4f054ba9fdadd0b4a771eaf79f109cea0d4f54f
sejin park <seijin0722@naver.com>
2024-03-20 10:34:42 +0900

    :memo: LV_Silver

--- silver/DP/숫자2xn타일링/Main.java
+++ silver/DP/숫자2xn타일링/Main.java
@@ -12,8 +12,8 @@ public class Main {
         dp = new int[N + 1];
         dp[0] = 0;
         dp[1] = 1;
-        dp[2] = 2;
-        if (N > 2) {
+        if (N > 1) {
+            dp[2] = 2;
             for (int i = 3; i <= N; i++) {
                 dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
             }
