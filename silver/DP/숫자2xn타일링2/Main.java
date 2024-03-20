8f39e69d7db7bb135e872205ec1059778dc7d30a
sejin park <seijin0722@naver.com>
2024-03-20 11:06:54 +0900

    :memo: LV_Silver

---
+++ silver/DP/숫자2xn타일링2/Main.java
@@ -0,0 +1,23 @@
+package silver.DP.숫자2xn타일링2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        dp = new int[N + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        if (N > 1) {
+            dp[2] = 3;
+            for (int i = 3; i <= N; i++) {
+                dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
