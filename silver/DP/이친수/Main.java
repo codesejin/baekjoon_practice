997d74f9dd0f08ea332fdcd05118c6b9aa7851ce
sejin park <seijin0722@naver.com>
2024-03-20 12:00:23 +0900

    :memo: LV_Silver

--- silver/DP/이친수/Main.java
+++ silver/DP/이친수/Main.java
@@ -8,11 +8,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int dp[] = new int[N + 1];
+        long dp[] = new long[N + 1];
         dp[1] = 1;
         if (N > 1) {
-            dp[2] = 1;
-            for (int i = 3; i <= N; i++) {
+            for (int i = 2; i <= N; i++) {
                 dp[i] = dp[i - 1] + dp[i - 2];
             }
         }
